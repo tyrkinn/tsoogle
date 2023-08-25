732f8c7189f48a8cacde5b855a9c17479bc0967a
Nikita Tyrkin <tyrkinn@pm.me>
2023-08-25 22:01:01 +0300

    Remove test file from git

--- demo.ts
+++
@@ -1,8 +0,0 @@
-export declare const add: (x: number, y: number) => number
-
-
-export function filter<T>(pred: (v: T) => boolean, lst: T[]): T[] {
-  return lst.filter(pred)
-}
-
-export function map<A, B>(fn: (x: A) => B, list: readonly A[]): B[];
